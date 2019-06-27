7a72ace2be8f62f4993690edcc2fdd960d372692
Yes <joshuayes@gmail.com>
2019-06-28 00:39:59 +0800

    Move to gatsby

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: "Yes Lee",
+    description: "Yes Lee is a frontend engineer in Taiwan.",
+    author: "Yes Lee",
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -21,10 +21,10 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#f7f9fa`,
+        theme_color: `#f7f9fa`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/favicon.ico`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
---
+++ src/containers/HomePage/index.js
@@ -0,0 +1,70 @@
+import React, { Component } from "react"
+import "./index.css"
+import profile from "./profile.jpg"
+import FAIcon from "../../components/FAIcon"
+
+class HomePage extends Component {
+  render() {
+    return (
+      <div className="app">
+        <img className="profile" src={profile} alt="profile" />
+        <h1 className="title">Yes Lee</h1>
+        <p>Frontend Engineer in Taiwan</p>
+        <p>
+          <a
+            href="http://hiking-itinerary.hiiamyes.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Hiking Itinerary Generator
+          </a>
+          {`: A website for hikers to plan their mountain hike itinerary in Taiwan.`}
+        </p>
+        <div>
+          <a
+            className="contact-link"
+            href="https://www.facebook.com/profile.php?id=100000254874042"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FAIcon icon={["fab", "facebook"]}></FAIcon>
+          </a>
+          <a
+            className="contact-link"
+            href="https://github.com/hiiamyes"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FAIcon icon={["fab", "github"]}></FAIcon>
+          </a>
+          <a
+            className="contact-link"
+            href="https://medium.com/@hiiamyes"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FAIcon icon={["fab", "medium-m"]}></FAIcon>
+          </a>
+          <a
+            className="contact-link"
+            href="https://www.youtube.com/playlist?list=PL0hFmSBG9QBEaBbQNdjdXoj3mo0TXJeZ9"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FAIcon icon={["fab", "youtube"]}></FAIcon>
+          </a>
+          <a
+            className="contact-link"
+            href="https://www.linkedin.com/in/tsung-yeh-lee-3a558172/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FAIcon icon={["fab", "linkedin"]}></FAIcon>
+          </a>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default HomePage
