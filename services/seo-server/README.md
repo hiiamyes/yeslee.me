a7ff1b2a9c8326651d0ef890a62d00dd73e327d1
yes <yes@emq.com>
2019-09-12 10:54:45 +0800

    wip

---
+++ services/seo-server/README.md
@@ -0,0 +1,5 @@
+seo server
+
+- puppeteer
+- [Rendertron](https://github.com/GoogleChrome/rendertron)
+- [rendertron-middleware](https://github.com/GoogleChrome/rendertron/tree/master/middleware)
---
+++ services/seo-server/src/index.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const rendertron = require("rendertron-middleware")
+const path = require("path")
+
+const app = express()
+
+app.use(
+  rendertron.makeMiddleware({
+    proxyUrl: "http://rendertron:3008/render",
+  })
+)
+
+app.use(express.static(path.resolve(__dirname, "../", "build")))
+app.get("*", (request, response) => {
+  response.sendFile(path.resolve(__dirname, "../", "build", "index.html"))
+})
+
+app.listen(process.env.PORT)
